eb7de36e0c55e86dfba5c84a6f1c61b238e2be82
Muhammad Hammad Shahid <hammad.shahid.pk@gmail.com>
2023-01-07 18:02:16 +0500

    settings form ui changes

--- components/SettingsForm.js
+++ components/SettingsForm.js
@@ -1,4 +1,4 @@
-import { Box, Grid, TextField, Typography } from "@mui/material";
+import { Box, Button, Grid, TextField, Typography } from "@mui/material";
 import React, { useState } from "react";
 import { useAccount } from "wagmi";
 
@@ -7,62 +7,77 @@ const SettingsForm = () => {
   const [bio, changeBio] = useState("");
   const [email, changeEmail] = useState("");
   const { address, isConnected } = useAccount();
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    console.log(name,bio,email);
+  }
   return (
     <>
       <Box px={10}>
         <Typography mb={6} variant="h1" color="text.primary">
           Profile Details
         </Typography>
-        <Grid container rowSpacing={5} columnSpacing={{ xs: 1, sm: 2, md: 9 }}>
-          <Grid item xs={6}>
-            <TextField
-              value={name}
-              fullWidth
-              label="Username"
-              id="Username"
-              color="secondary"
-              placeholder="UserName"
-              onInput={(e) => changeName(e.target.value)}
-            />
+        <form onSubmit={(e)=>handleSubmit(e)}>
+          <Grid
+            container
+            rowSpacing={5}
+            columnSpacing={{ xs: 1, sm: 2, md: 9 }}
+          >
+            <Grid item xs={6}>
+              <TextField
+                value={name}
+                fullWidth
+                label="Username"
+                id="Username"
+                color="secondary"
+                placeholder="Username"
+                onInput={(e) => changeName(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                value={bio}
+                multiline
+                maxRows={4}
+                fullWidth
+                label="Bio"
+                id="Bio"
+                color="secondary"
+                placeholder="Bio"
+                onInput={(e) => changeBio(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                value={email}
+                fullWidth
+                label="Email"
+                id="Email"
+                color="secondary"
+                placeholder="Email"
+                onInput={(e) => changeEmail(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                fullWidth
+                value={address}
+                color="secondary"
+                label="Wallet Address"
+                id="WalletAddress"
+                placeholder="Wallet Address"
+              />
+            </Grid>
+            {
+              //social connections remaining
+            }
+            <Grid item xs={6}>
+              <Button type="submit" variant="outlined" color="secondary">
+                Save Changes
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item xs={6}>
-            <TextField
-              value={bio}
-              multiline
-              maxRows={4}
-              fullWidth
-              label="Bio"
-              id="Bio"
-              color="secondary"
-              placeholder="Bio"
-              onInput={(e) => changeBio(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              value={email}
-              fullWidth
-              label="Email"
-              id="Email"
-              color="secondary"
-              placeholder="Email"
-              onInput={(e) => changeEmail(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              fullWidth
-              value={address}
-              disabled
-              label="Wallet Address"
-              id="WalletAddress"
-              placeholder="Wallet Address"
-            />
-          </Grid>
-          {
-            //social connections remaining
-          }
-        </Grid>
+        </form>
       </Box>
     </>
   );
--- pages/account/settings.js
+++ pages/account/settings.js
@@ -1,9 +1,13 @@
 import { Box, Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 import SettingsForm from "../../components/SettingsForm";
 import FullLayout from "../../src/layouts/FullLayout";
 
+
 const Settings = () => {
+    useEffect(()=>{
+        fetch("/api/hello").then((res)=>res.json()).then((data)=>console.log(data))
+    })
   return (
     <>
       <FullLayout check={"user"}>
