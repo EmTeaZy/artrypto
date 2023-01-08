de4db6627317e8a032b528bfbe13d177cff0bd8b
talha <talhabhai8@gmail.com>
2023-01-08 23:29:00 +0500

    User list displayed Admin navbar color changed Padding removed

--- components/NavbarComponents/ConnectStatus.js
+++ components/NavbarComponents/ConnectStatus.js
@@ -1,16 +1,19 @@
-import { Typography } from '@mui/material'
+import {Typography} from '@mui/material'
 import React from 'react'
-import { useAccount } from 'wagmi'
+import {useAccount} from 'wagmi'
 import CheckSharpIcon from '@mui/icons-material/CheckSharp';
 import ClearSharpIcon from '@mui/icons-material/ClearSharp';
+
 const ConnectStatus = () => {
-    const {address,isConnected}=useAccount()
-  return (
-   <>
-     {isConnected?<CheckSharpIcon fontSize='small' color="success"/>:<ClearSharpIcon fontSize='small' color="danger"/>}
-     <Typography color={isConnected?"text.success":"text.danger"} variant="connectStatus">{isConnected?"connected":"not connected"}</Typography>
-   </>
-  )
+    const {isConnected} = useAccount()
+    return (
+        <>
+            {isConnected ? <CheckSharpIcon fontSize='small' color="success"/> :
+                <ClearSharpIcon fontSize='small' color="danger"/>}
+            <Typography color={isConnected ? "text.success" : "text.danger"}
+                        variant="connectStatus">{isConnected ? "Connected" : "Not connected"}</Typography>
+        </>
+    )
 }
 
 export default ConnectStatus
--- src/layouts/footer/Footer.js
+++ src/layouts/footer/Footer.js
@@ -4,7 +4,7 @@ import {Box, Typography} from "@mui/material";
 const Footer = () => {
   return (
     <Box sx={{ p: 3, textAlign: "center" }}>
-      <Typography>
+      <Typography variant="h6">
         © 2022 All rights reserved by Artrypto
       </Typography>
     </Box>
