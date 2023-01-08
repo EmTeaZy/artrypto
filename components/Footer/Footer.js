717f13f671585095eff727936d9c85b1a9a2a72e
Hamza Jamil <lyallpurlad@gmail.com>
2023-01-09 00:06:55 +0500

    Sticky Footer Added

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
 
-const Footer = () => {
+function Copyright() {
   return (
-    <div>
-        <h1>Footer In Progress</h1> 
-    </div>
-  )
+    <Typography variant="body2" color="text.secondary">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default Footer
+export default function StickyFooter() {
+  return (
+      <Box
+        component="footer"
+        sx={{
+          py: 3,
+          px: 2,
+          mt: 'auto',
+          backgroundColor: (theme) =>
+            theme.palette.mode === 'light'
+              ? theme.palette.grey[200]
+              : theme.palette.grey[800],
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography variant="body1">
+            My sticky footer can be found here.
+          </Typography>
+          <Copyright />
+        </Container>
+      </Box>
+  );
+}
