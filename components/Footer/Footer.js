ca10b7071e32820e401bf0fd8b0eaab018b11c56
Hamza Jamil <lyallpurlad@gmail.com>
2023-01-09 09:31:03 +0500

    Footer fix

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -31,7 +31,7 @@ export default function StickyFooter() {
       sx={{
         display: 'flex',
         flexDirection: 'column',
-        minHeight: '100vh',
+        minHeight: '23vh',
 
       }}
     >
