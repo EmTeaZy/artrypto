604e088fab60934034489d0fc313e77c0b0d7cf6
Hamza Jamil <lyallpurlad@gmail.com>
2023-03-31 09:34:12 +0500

    Add User Test Case

--- __tests__/add.user.test.js
+++ __tests__/add.user.test.js
@@ -12,7 +12,7 @@ describe('User API', () => {
         jest.clearAllMocks();
     });
 
-    test('should create a new user', async () => {
+    test('Test To Check If It Adds User', async () => {
         const userData = {
             username: 'testuser',
             email: 'testuser@example.com',
@@ -29,27 +29,5 @@ describe('User API', () => {
         expect(res._getStatusCode()).toBe(400);
     });
 
-    test('should not create a new user', async () => {
-        const userData = {
-            username: 'testuser',
-            email: 'testuser@example.com',
-            bio: 'Test user bio',
-            walletAddress: '0x1234567890abcdef',
-            imgid: '12345',
-            links: {
-                website: 'https://example.com',
-                twitter: 'https://twitter.com/example'
-            }
-        };
 
-        const {req, res} = createMocks({
-            method: 'POST',
-            body: userData,
-        });
-
-        await postRequest(req, res);
-
-        expect(connectDb).toHaveBeenCalledTimes(1);
-        expect(res._getStatusCode()).toBe(400);
-    });
 });
