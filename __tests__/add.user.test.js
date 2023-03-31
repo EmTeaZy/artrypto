5daab55327e3b127c62982796c7ed9747a411729
Hamza Jamil <lyallpurlad@gmail.com>
2023-03-31 09:35:45 +0500

    test case for not creating user

--- __tests__/add.user.test.js
+++ __tests__/add.user.test.js
@@ -29,5 +29,28 @@ describe('User API', () => {
         expect(res._getStatusCode()).toBe(400);
     });
 
+    test('test to check if it does not create user', async () => {
+        const userData = {
+            username: 'testuser',
+            email: 'testuser@example.com',
+            bio: 'Test user bio',
+            walletAddress: '0x1234567890abcdef',
+            imgid: '12345',
+            links: {
+                website: 'https://example.com',
+                twitter: 'https://twitter.com/example'
+            }
+        };
+
+        const {req, res} = createMocks({
+            method: 'POST',
+            body: userData,
+        });
+
+        await postRequest(req, res);
+
+        expect(connectDb).toHaveBeenCalledTimes(1);
+        expect(res._getStatusCode()).toBe(400);
+    });
 
 });
